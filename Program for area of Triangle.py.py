61103ff1fb38749983e1851a7b913ed80b947343
Yogesh051 <114800361+Yogesh051@users.noreply.github.com>
2022-10-05 20:09:25 +0530

    Add files via upload

---
+++ Program for area of Triangle.py.py
@@ -0,0 +1,13 @@
+import math
+from re import S
+a = int(input("length of first side"))
+b = int(input("length of second side"))
+c = int(input("length of third side"))
+
+#s=sum of all sides(perimeter of triangle)
+
+s=(a+b+c)/2
+
+print("perimeter of triangle/2:",s)
+area = math.sqrt(s*(s-a)*(s-b)*(s-c))
+print("area is:",area)
